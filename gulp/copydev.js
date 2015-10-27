90e24d89037872d5305174bb5210b1b1b232a19c
Dan Osborne <danosborne@rocketmail.com>
2015-10-27 11:13:50 +0000

    renamed files

--- gulp/copydev.js
+++ gulp/copydev.js
@@ -1,8 +1,14 @@
 //Copy dev files into src folder
 // ---------------------------------------------------
-var gulp = require('gulp');
+var gulp  = require('gulp');
+var clean = require('gulp-clean');
 
-gulp.task('copy', ['concat'], function(){
+gulp.task('clean', function(){
+	return gulp.src('./src', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('copy', ['concat', 'clean'], function(){
     return gulp.src('./dev/src/**/*.scss')
       .pipe(gulp.dest('./src'));
 });
